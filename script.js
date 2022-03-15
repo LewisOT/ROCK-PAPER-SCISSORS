135ac8426ecf8a13d2257a59fd2a9a15b638a0a6
Vlolsen90 <98793899+v-olsen@users.noreply.github.com>
2022-03-15 14:45:07 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
+
+const playerSelection = prompt("Rock, paper or scissor?").toLowerCase();
+const computerSelection = computerPlay();
 function computerPlay() {
     let choice = "";
     let randomNumber = Math.floor(Math.random() * (3)) + 1;
@@ -12,4 +15,43 @@ function computerPlay() {
     }
     return choice
 }
-console.log(computerPlay())
+/*const playerSelection = prompt("Rock, paper or scissor?").toLowerCase; */
+
+
+
+function playRound(playerSelection, computerSelection) {
+    if (playerSelection === "rock") {
+        if (computerSelection === "scissors") {
+            return "You win! Rock beats scissors!"
+        }
+        else if (computerSelection === "paper") {
+            return "You lose! Paper beats rock!"
+        }
+        else {
+            return "Draw!"
+        }
+    }
+    if (playerSelection === "scissors") {
+        if (computerSelection ==="rock") {
+            return "You lose! Rock beats scissors"
+        }
+        else if (computerSelection === "paper") {
+            return "You win! Scissors beats paper!"
+        }
+        else {
+            return "Draw!"
+        }
+    }
+    if (playerSelection === "paper") {
+        if (computerSelection === "rock") {
+            return "You win! Paper beats rock!"
+        }
+        else if (computerSelection === "scissors") {
+            return "You lose! Scissors beats paper"
+        }
+        else {
+            return "Draw!"
+        }
+    }
+}
+console.log(playRound(computerSelection, playerSelection))
