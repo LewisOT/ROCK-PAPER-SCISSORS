1e8ac8d43d6488f4cd885ef14ebf5fc13d9e713c
Vlolsen90 <98793899+v-olsen@users.noreply.github.com>
2022-03-15 19:00:13 -0500

    Added playler scores. Created the main function to run the program.

--- script.js
+++ script.js
@@ -1,57 +1,82 @@
-
-const playerSelection = prompt("Rock, paper or scissor?").toLowerCase();
-const computerSelection = computerPlay();
 function computerPlay() {
-    let choice = "";
-    let randomNumber = Math.floor(Math.random() * (3)) + 1;
-    if (randomNumber === 1) {
-        choice = "rock"
-    }
-    else if (randomNumber == 2) {
-        choice = "paper"
-    }
-    else {
-        choice = "scissors"
-    }
-    return choice
+  let choice = ""
+  let randomNumber = Math.floor(Math.random() * 3) + 1
+  if (randomNumber === 1) {
+    choice = "rock"
+  } else if (randomNumber == 2) {
+    choice = "paper"
+  } else {
+    choice = "scissors"
+  }
+  return choice
 }
-/*const playerSelection = prompt("Rock, paper or scissor?").toLowerCase; */
+//const playerSelection = prompt("Rock, paper or scissor?").toLowerCase()
 
-
-
+/*const playerSelection = "rock"
+const computerSelection = computerPlay()*/
 function playRound(playerSelection, computerSelection) {
-    if (playerSelection === "rock") {
-        if (computerSelection === "scissors") {
-            return "You win! Rock beats scissors!"
-        }
-        else if (computerSelection === "paper") {
-            return "You lose! Paper beats rock!"
-        }
-        else {
-            return "Draw!"
-        }
+  let stringOut = ""
+  if (playerSelection === "rock") {
+    if (computerSelection === "scissors") {
+      return "You win! Rock beats scissors!"
+    } else if (computerSelection === "paper") {
+      return "You lose! Paper beats rock!"
+    } else {
+      return "Draw!"
     }
-    if (playerSelection === "scissors") {
-        if (computerSelection ==="rock") {
-            return "You lose! Rock beats scissors"
-        }
-        else if (computerSelection === "paper") {
-            return "You win! Scissors beats paper!"
-        }
-        else {
-            return "Draw!"
-        }
+  }
+  if (playerSelection === "scissors") {
+    if (computerSelection === "rock") {
+      return "You lose! Rock beats scissors!"
+    } else if (computerSelection === "paper") {
+      return "You win! Scissors beats paper!"
+    } else {
+      return "Draw!"
     }
-    if (playerSelection === "paper") {
-        if (computerSelection === "rock") {
-            return "You win! Paper beats rock!"
-        }
-        else if (computerSelection === "scissors") {
-            return "You lose! Scissors beats paper"
-        }
-        else {
-            return "Draw!"
-        }
+  }
+  if (playerSelection === "paper") {
+    if (computerSelection === "rock") {
+      return "You win! Paper beats rock!"
+    } else if (computerSelection === "scissors") {
+      return "You lose! Scissors beats paper"
+    } else {
+      return "Draw!"
     }
+  }
 }
-console.log(playRound(computerSelection, playerSelection))
+
+function game() {
+  let playerScore = 0
+  let computerScore = 0
+  for (let i = 0; i < 5; i++) {
+    const playerSelection = prompt("Rock, paper or scissor?").toLowerCase()
+    const computerSelection = computerPlay()
+    let outCome = playRound(playerSelection, computerSelection)
+    if (outCome.includes("win")) {
+      playerScore += 1
+    } else if (outCome.includes("lose")) {
+      computerScore += 1
+    }
+  }
+  if (playerScore > computerScore) {
+    return (
+      "You win! The final score is: " +
+      "Player: " +
+      playerScore +
+      " and " +
+      "Computer: " +
+      computerScore
+    )
+  } else {
+    return (
+      "You Lose! The final score is: " +
+      "Computer: " +
+      computerScore +
+      " and " +
+      "Player: " +
+      playerScore
+    )
+  }
+}
+
+console.log(game())
